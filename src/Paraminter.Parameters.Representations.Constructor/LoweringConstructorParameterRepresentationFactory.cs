ac6cd9b74554057c798da7cfa4b9326a877ec377
ErikWe <erikwestberg7@gmail.com>
2024-05-02 18:11:58 +0200

    Add implementation of IParameterRepresentationFactory

---
+++ src/Paraminter.Parameters.Representations.Constructor/LoweringConstructorParameterRepresentationFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace Paraminter.Parameters.Representations;
+
+using System;
+
+/// <summary>Handles creation of <see cref="IConstructorParameterRepresentation"/> using <see cref="IConstructorParameter"/>.</summary>
+public sealed class LoweringConstructorParameterRepresentationFactory : IParameterRepresentationFactory<IConstructorParameter, IConstructorParameterRepresentation>
+{
+    private readonly IConstructorParameterRepresentationFactory InnerFactory;
+
+    /// <summary>Instantiates a <see cref="LoweringConstructorParameterRepresentationFactory"/>, handling creation of <see cref="IConstructorParameterRepresentation"/> using <see cref="IConstructorParameter"/>.</summary>
+    /// <param name="innerFactory">Handles creation of <see cref="IConstructorParameterRepresentation"/> using the indices and names of type parameters.</param>
+    public LoweringConstructorParameterRepresentationFactory(IConstructorParameterRepresentationFactory innerFactory)
+    {
+        InnerFactory = innerFactory ?? throw new ArgumentNullException(nameof(innerFactory));
+    }
+
+    IConstructorParameterRepresentation IParameterRepresentationFactory<IConstructorParameter, IConstructorParameterRepresentation>.Create(IConstructorParameter parameter)
+    {
+        if (parameter is null)
+        {
+            throw new ArgumentNullException(nameof(parameter));
+        }
+
+        return InnerFactory.Create(parameter.Symbol.Name);
+    }
+}
